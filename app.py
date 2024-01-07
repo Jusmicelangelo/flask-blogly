f575afbb98c5665a6dc0c3167ce7d36c1e87e93b
Jusmicelangelo <jusmicelangelo@Jusmicelangelos-MacBook-Pro.local>
2024-01-07 13:54:50 +0100

    in between

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
 """Blogly application."""
 
-from flask import Flask
-from models import db, connect_db
+from flask import Flask, redirect, render_template
+from models import db, connect_db, User
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = 'ihaveasecret'
 
 connect_db(app)
+app.app_context().push()
 db.create_all()
+
+@app.route('/')
+def root():
+    """Homepage redirects to list of users."""
+
+    return redirect("/users")
+
+@app.route('/users')
+def users():
+    """Shows a complete list of users"""
+    users = User.query.order_by(User.last_name, User.first_name).all()
+    return render_template("/users", users = users)
--- models.py
+++ models.py
@@ -1,1 +1,39 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
 """Models for Blogly."""
+
+DEFAULT_IMAGE = "https://unsplash.com/de/fotos/ein-3d-bild-eines-mannes-mit-roten-linien-auf-seinem-korper-WWQzZO6kORA"
+
+class User(db.Model):
+    """User."""
+
+    __tablename__ = "users"
+
+    def __repr__(self):
+        u = self
+        return f"<User: {u.first_name} {u.last_name}>"
+
+    id = db.Column (db.Integer,
+                    primary_key = True,
+                    autoincrement = True)
+    
+    first_name = db.Column (db.Text,
+                            nullable = False)
+    
+    last_name = db.Column (db.Text,
+                           nullable = False)
+    
+    profile_image = db.Column (db.Text,
+                               nullable = False,
+                               default = DEFAULT_IMAGE)
+    
+    def greet(self):
+        return f"Logged in as {self.first_name} {self.last_name}"
+    
+def connect_db(app):
+    """Connect to database"""
+    db.app = app
+    db.init_app(app)
+
